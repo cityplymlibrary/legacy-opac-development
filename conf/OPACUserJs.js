5d08ebc188082d8c8b5f1bafce945baff441b7de
Jake Deery <72008576+jdeery-cityplym@users.noreply.github.com>
2021-07-02 11:38:29 +0100

    * Synced with master development repo - consider this the final commit to this legacy repo ... :-( & :-)

--- conf/OPACUserJs.js
+++ conf/OPACUserJs.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 	//
 	// gdpr
-	if(localStorage.getItem('-ccp-gdrp-dismissed') == null) $('body').prepend('<div id=\"-gdpr-banner\"><p>This website uses cookies to ensure you get the best experience on our website <a id=\"-gdpr-moreinfo\" href=\"\/\/www.cityplym.ac.uk\/legal-cookies\/\" target=\"_blank\">More info \u00BB<\/a><button id=\"-gdpr-dismiss\" class=\"btn btn-primary\">Dismiss<\/button><\/p><\/div>');
+	if(localStorage.getItem('-ccp-gdrp-dismissed') == null) $('body').prepend('<div id=\"-gdpr-banner\"><p>This website uses cookies to ensure you get the best experience on our website <a id=\"-gdpr-moreinfo\" href=\"#!\/privacy-policy\">More info \u00BB<\/a><button id=\"-gdpr-dismiss\" class=\"btn btn-primary\">Dismiss<\/button><\/p><\/div>');
 	$('#-gdpr-dismiss, #-gdpr-moreinfo').click(function() {
 		localStorage.setItem('-ccp-gdrp-dismissed', 'true');
 		$('#-gdpr-banner').remove();
@@ -12,18 +12,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	// site-wide logic (MAIN FUNC)
 
 	// nav menus
-	if($('.sco').length == 0) $('ul.nav').first().html('<li><a target=\"_blank\" href=\"https:\/\/moodle.cityplym.ac.uk\/course\/view.php?id=399\" title=\"Name &amp; subject search\" id=\"helpmenu\"><i class=\"fa fa-info-circle fa-icon-white\"><\/i> <span class=\"helplabel\">Help<\/span><\/a><\/li><li><a href=\"\/cgi-bin\/koha\/opac-authorities-home.pl\" title=\"Name &amp; subject search\" id=\"ssmenu\"><i class=\"fa fa-graduation-cap fa-icon-white\"><\/i> <span class=\"sslabel\">Name &amp; subject search<\/span><\/a><\/li><li><a href=\"\/cgi-bin\/koha\/opac-course-reserves.pl\" title=\"Lists\" id=\"listsmenu\"><i class=\"fa fa-list fa-icon-white\"><\/i> <span class=\"listslabel\">Reading lists<\/span><\/a><\/li><li><a href=\"\/cgi-bin\/koha\/opac-shelves.pl?op=list&amp;category=2\" title=\"Lists\" id=\"listsmenu2\"><i class=\"fa fa-list-alt fa-icon-white\"><\/i> <span class=\"listslabel2\">Other lists<\/span><\/a><\/li>');
+	$('.navbar-inner ul.nav').first().html('<li><a target=\"_blank\" href=\"https:\/\/moodle.cityplym.ac.uk\/course\/view.php?id=399\" title=\"Help\" id=\"helpmenu\"><i class=\"fa fa-info-circle fa-icon-white\"><\/i> <span class=\"helplabel\">Help<\/span><\/a><\/li><li><a target=\"_blank\" href=\"https:\/\/forms.gle\/2NKzKTWrzHtcanuL7\" title=\"Feedback\" id=\"feedbackmenu\"><i class=\"fa fa-comments fa-icon-white\"><\/i> <span class=\"feedbacklabel\">Feedback<\/span><\/a><\/li><li><a href=\"\/cgi-bin\/koha\/opac-authorities-home.pl\" title=\"Name &amp; subject search\" id=\"ssmenu\"><i class=\"fa fa-graduation-cap fa-icon-white\"><\/i> <span class=\"sslabel\">Name &amp; subject search<\/span><\/a><\/li><li><a href=\"\/cgi-bin\/koha\/opac-course-reserves.pl\" title=\"Lists\" id=\"listsmenu\"><i class=\"fa fa-list fa-icon-white\"><\/i> <span class=\"listslabel\">Reading lists<\/span><\/a><\/li><li><a href=\"\/cgi-bin\/koha\/opac-shelves.pl?op=list&amp;category=2\" title=\"Virtual shelves\" id=\"listsmenu2\"><i class=\"fa fa-bookmark fa-icon-white\"><\/i> <span class=\"listslabel2\">Virtual shelves<\/span><\/a><\/li>');
 
 	// login modal
-	$('#loginModal').html('<div class=\"modal-header\"><button type=\"button\" class=\"closebtn\" data-dismiss=\"modal\" aria-hidden=\"true\">\u00D7<\/button><h2 id=\"modalLoginLabel\">Log in to your account<\/h2><\/div><div id=\"modalAuth\" class=\"modal-body\"><h3>College student?<\/h3><p><a href=\"\/Shibboleth.sso\/Login?target=https:\/\/ccp.koha-ptfs.co.uk'+window.location.pathname+window.location.search+'\" class=\"btn btn-primary\">Go to College login &raquo;<\/a><\/p><h3>Staff or associate?<\/h3><p><a href=\"\/cgi-bin\/koha\/opac-user.pl\" class=\"btn btn-primary\">Go to local Koha login &raquo;<\/a><\/p><\/div>');
+	$('#loginModal').html('<div class=\"modal-header\"><button type=\"button\" class=\"closebtn\" data-dismiss=\"modal\" aria-hidden=\"true\">\u00D7<\/button><h2 id=\"modalLoginLabel\">Log in to your account<\/h2><\/div><div id=\"modalAuth\" class=\"modal-body\"><h3>College student or staff?<\/h3><p><a href=\"\/Shibboleth.sso\/Login?target=https:\/\/ccp.koha-ptfs.co.uk\/cgi-bin\/koha\/opac-user.pl\" class=\"btn btn-primary\">Go to College login &raquo;<\/a><\/p><h3>College associate?<\/h3><p><a href=\"\/cgi-bin\/koha\/opac-user.pl\" class=\"btn btn-primary\">Go to local Koha login &raquo;<\/a><\/p><\/div>');
 
 	// search box
 	$('#translControl1').attr('placeholder', 'Search for books, eBooks, journal articles, DVDs...');
 
 	// moresearches
-	$('#moresearches ul').html('<li><a href=\"\/cgi-bin\/koha\/opac-search.pl\">Advanced search<\/a><\/li>');
+	$('#moresearches').html('<ul><li><a href=\"\/cgi-bin\/koha\/opac-search.pl\">Advanced search<\/a><\/li><\/ul>');
 
 	// login page
-	$('#opac-auth').html('<h2>Log in to your account<\/h2><h3>College student?<\/h3><p><a href=\"\/Shibboleth.sso\/Login?target=https:\/\/ccp.koha-ptfs.co.uk'+window.location.pathname+window.location.search+'\" class=\"btn btn-primary\">Go to College login &raquo;<\/a><\/p><h3>Staff or associate?<\/h3><form action=\"\/cgi-bin\/koha\/opac-user.pl\" name=\"auth\" id=\"auth\" method=\"post\" autocomplete=\"off\"><input type=\"hidden\" name=\"koha_login_context\" value=\"opac\"\/><fieldset class=\"brief\"><label for=\"userid\">Login<\/label><input type=\"text\" size=\"25\" id=\"userid\" name=\"userid\"\/><label for=\"password\">Password<\/label><input type=\"password\" size=\"25\" id=\"password\" name=\"password\"\/><\/fieldset><input type=\"submit\" value=\"Log in\" class=\"btn btn-primary\"\/><div id=\"nologininstructions\"><p>&nbsp;<\/p><p><strong>Don\'t have a password yet?<\/strong><\/p><p>If you don\'t have a password yet, stop by the Library counter the next time you\'re in College. We\'ll happily set one up for you.<\/p><p><strong>Don\'t have a student or staff ID card?<\/strong><\/p><p>If you don\'t have a student or staff ID card, please come and see us and we can provide you with one. Remember to bring valid photo ID.<\/p><\/div><\/form>');
-
+	if($('#auth').length > 0 && $('#opac-auth').children('h4').eq(1).length > 0) {
+		$('#opac-auth').children('h4').eq(0).text('College student or staff?');
+		$('#opac-auth').children('p').eq(0).html('<a href=\"\/Shibboleth.sso\/Login?target=https:\/\/ccp.koha-ptfs.co.uk\/cgi-bin\/koha\/opac-main.pl\" class=\"btn btn-primary\">Go to College login &raquo;<\/a>');
+		$('#opac-auth').children('h4').eq(1).text('College associate?');
+		$('#opac-auth').children('p').eq(1).text('Please enter the login details provided to you by the Library team');
+		$('#nologininstructions').html('<p style=\"padding:1rem 0\">If you don\'t have a College ID card or password, visit the Library counter, where we will be able to provide these to you. Please note: if you need a College ID, please bring photo ID and proof of enrolment.<\/p>');
+	} else if($('#auth').length > 0 && $('#opac-auth').children('h4').eq(1).length < 1) {
+		$('#opac-auth').children('h4').eq(0).text('College associate?');
+		$('#opac-auth').children('p').eq(0).text('Please enter the login details provided to you by the Library team');
+		$('#nologininstructions').html('<p style=\"padding:1rem 0\">If you don\'t have a College ID card or password, visit the Library counter, where we will be able to provide these to you. Please note: if you need a College ID, please bring photo ID and proof of enrolment.<\/p>');
+	}
 });
